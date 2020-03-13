e0a9dc1828e8fab0eed36226cc05e5bbaee6cad9
aaronk6 <aaronk6@users.noreply.github.com>
2020-03-13 15:17:15 +0100

    Copyright 2020

--- Cardano.lua
+++ Cardano.lua
@@ -4,7 +4,7 @@
 -- Username: DdzFFzCqrhf..., DdzFFzCqrhs...
 -- Password: (empty)
 --
--- Copyright (c) 2018 aaronk6
+-- Copyright (c) 2020 aaronk6
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
